3041badca8b837eaa358b6c3187d13fac1581d26
abba <abba@nuclearinstruments.eu>
2023-03-15 20:03:25 +0100

    test 19 aggiornato

--- test19.py
+++ test19.py
@@ -18,95 +18,91 @@ for ip in DGZ_IP:
     try:
         sdks[-1].connect(ip)
         print ("Digitizer %s connesso" % ip)
-        test_report["dgtz"][i]["connection"] = True
     except:
         print ("Digitizer %s non raggiungibile" % ip)
         sdks.pop()
         failed = True
     
 for sdk in sdks:
+
+    s=sdk.get_parameter("dgtz.info.section")
+
+    sdk.set_parameter("dgtz.send_delay", "0")
+    # digitizer configuration
+    #set signal polarity to negative "pos" / "neg"
+    sdk.set_parameter("in.polarity", "neg")
+    # pre-trigger buffer len (us)
+    sdk.set_parameter("dgtz.pre", 0.5)
+    # post-trigger acquisition buffer (us)
+    sdk.set_parameter("dgtz.post", 20)
+    # delay on external trigger (us)
+    sdk.set_parameter("dgtz.trg_delay", 0)
+    # trigger source: "ext_trigger", "self_le", "self_de", "periodic", "manual", "lemo_0"
+    sdk.set_parameter("trg.mode", "self_le")
+    # internal trigger mode: "or", "and2", "and"
+    sdk.set_parameter("trg.self_coinc", "or")
+    # periodic trigger rate (Hz)
+    sdk.set_parameter("trg.self_rate", 50)
+    # trigger inibition (ns)
+    sdk.set_parameter("trg.trigger_inib", 10)
+    # parameter those are different for each channel of the digitizer
+    for i in range(0, 8):
+        # trigger threshold (LSB)
+        sdk.set_parameter("trg.threshold", 2000, i)
+        # trigger mask (LSB)
+        sdk.set_parameter("trg.mask", 0, i)
+        # digital input offset
+        #sdk.set_parameter("in.offset", -1350, i)
+        sdk.set_parameter("in.offset", 0, i)
+        # trigger threshold (LSB)
+        sdk.set_parameter("in.chmap", i, i)
+    
     try:
-        s=sdk.get_parameter("dgtz.info.section")
-        test_report["dgtz"][i]["section"] = int(float(s))
-
-        sdk.set_parameter("dgtz.send_delay", "0")
-        # digitizer configuration
-        #set signal polarity to negative "pos" / "neg"
-        sdk.set_parameter("in.polarity", "neg")
-        # pre-trigger buffer len (us)
-        sdk.set_parameter("dgtz.pre", 0.5)
-        # post-trigger acquisition buffer (us)
-        sdk.set_parameter("dgtz.post", 20)
-        # delay on external trigger (us)
-        sdk.set_parameter("dgtz.trg_delay", 0)
-        # trigger source: "ext_trigger", "self_le", "self_de", "periodic", "manual", "lemo_0"
-        sdk.set_parameter("trg.mode", "self_le")
-        # internal trigger mode: "or", "and2", "and"
-        sdk.set_parameter("trg.self_coinc", "or")
-        # periodic trigger rate (Hz)
-        sdk.set_parameter("trg.self_rate", 50)
-        # trigger inibition (ns)
-        sdk.set_parameter("trg.trigger_inib", 10)
-        # parameter those are different for each channel of the digitizer
-        for i in range(0, 8):
-            # trigger threshold (LSB)
-            sdk.set_parameter("trg.threshold", 2000, i)
-            # trigger mask (LSB)
-            sdk.set_parameter("trg.mask", 0, i)
-            # digital input offset
-            #sdk.set_parameter("in.offset", -1350, i)
-            sdk.set_parameter("in.offset", 0, i)
-            # trigger threshold (LSB)
-            sdk.set_parameter("in.chmap", i, i)
-        
-        try:
-            sdk.execute_cmd("configure_dgtz")
-        except Exception as e:
-            failed = True
-            print("Error executing digitizer{i} programming:" + str(e))
-
-        try:
-            sdk.execute_cmd("start_acquisition")
-        except Exception as e:
-            failed = True
-            print("Error executing digitizer{i} start:" + str(e))
-   
-        for j in range(0,100):
-            W=sdk.read_data("get_waveforms")
-            if W is not None:
-                for ch in range(0,8):
-                    wave = W["data"]["wave"][ch]
-                    
-                    # culate average, and averaged peak to peak
-                    avg = np.average(wave)
-                    p2p = np.max(wave) - np.min(wave)
-                    rms = np.sqrt(np.mean(wave**2))
-
-                    dt = 1.0
-                    rising_edges = np.where(np.diff(wave) > 0.1)[0]  # Indici dei fronti di salita con tolleranza del 10%
-                    rise_times = np.zeros(len(rising_edges))  # Array per memorizzare i rise time
-                    for i, edge in enumerate(rising_edges):
-                        # Trova il tempo in cui il segnale sale dal 10% al 90% del suo valore massimo con tolleranza del 10%
-                        threshold = 0.1 * (wave[edge+1] - wave[edge]) + wave[edge]
-                        idx_start = np.where(wave[edge:] >= threshold)[0][0] + edge
-                        threshold = 0.9 * (wave[edge+1] - wave[edge]) + wave[edge]
-                        idx_end = np.where(wave[edge:] >= threshold)[0][0] + edge
-                        rise_times[i] = (idx_end - idx_start) * dt  # Calcola il rise time
-                    
-                    # Calcola il tempo medio di salita
-                    rise_time = np.average(rise_times)
-
-                    test_report["dgtz"][i]["rms"] = rms
-                    test_report["dgtz"][i]["avg"] = avg
-                    test_report["dgtz"][i]["p2p"] = p2p
-                    test_report["dgtz"][i]["rise_times"] = rise_times
-                    test_report["dgtz"][i]["rise_time"] = rise_time
-                    test_report["dgtz"][i]["wave"] = wave
-
-    except:
-        #print error mesagge and which function generate it
-        print ("Errore durante la lettura dei parametri")
+        sdk.execute_cmd("configure_dgtz")
+    except Exception as e:
         failed = True
+        print("Error executing digitizer{i} programming:" + str(e))
+
+    
+    for j in range(0,100):
+        W=sdk.read_data("get_waveforms")
+        if W is not None:
+            for ch in range(0,8):
+                wave = W["wave"][ch]
+                nwave = np.array(wave)
+                # culate average, and averaged peak to peak
+                avg = np.average(nwave)
+                p2p = np.max(nwave) - np.min(nwave)
+          
+                rms = np.sqrt(np.mean(nwave **2))
+
+                dt = 1.0
+                rising_edges = np.where(np.diff(nwave) > 500)[0]  # Indici dei fronti di salita con tolleranza del 10%
+                rise_times = np.zeros(len(rising_edges))  # Array per memorizzare i rise time
+                for i, edge in enumerate(rising_edges):
+                    # Trova il tempo in cui il segnale sale dal 10% al 90% del suo valore massimo con tolleranza del 10%
+                    threshold = 0.1 * (nwave[edge+10] - nwave[edge-10]) + nwave[edge-10]
+                    idx_start = np.where(nwave[edge-10:edge+10] >= threshold)[0][0] + edge-10
+                    threshold = 0.9 * (nwave[edge+10] - nwave[edge-10]) + nwave[edge-10]
+                    idx_end = np.where(nwave[edge-10:edge+10] >= threshold)[0][0] + edge-10
+                    rise_times[i] = (idx_end - idx_start) * dt  # Calcola il rise time
+                
+                # Calcola il tempo medio di salita
+                rise_time = np.average(rise_times)
+                #calcolo il periodo dell'onda di ingresso dai rising edges
+                period = np.average(np.diff(rising_edges[2:]))
+                frequency = 1/period * 10e9
+                print ("Channel %d" % ch)
+                print ("Average: %.2f" % avg)
+                print ("Peak to peak: %.2f" % p2p)
+                print ("RMS: %.2f" % rms)
+                print ("Frequency: %.2f" %frequency)
+                print("Rise time: %.2f ns" % rise_time)
+
+
+
+
+
 
     i=i+1
 # salva il report in json
