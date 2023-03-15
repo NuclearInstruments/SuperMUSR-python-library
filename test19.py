53c5aa4e4deaeae3279c27cea1025a08f817a6fa
abba <abba@nuclearinstruments.eu>
2023-03-15 20:16:20 +0100

    plot su 19

--- test19.py
+++ test19.py
@@ -3,6 +3,7 @@ import json
 import time
 from test_defs import *
 import numpy as np
+import matplotlib.pyplot as plt
 
 print ("Acquisizione segnale")
 
@@ -64,44 +65,70 @@ for sdk in sdks:
         print("Error executing digitizer{i} programming:" + str(e))
 
     
-    for j in range(0,100):
-        W=sdk.read_data("get_waveforms")
-        if W is not None:
-            for ch in range(0,8):
-                wave = W["wave"][ch]
-                nwave = np.array(wave)
-                # culate average, and averaged peak to peak
-                avg = np.average(nwave)
-                p2p = np.max(nwave) - np.min(nwave)
-          
-                rms = np.sqrt(np.mean(nwave **2))
+    #for j in range(0,100):
+    W=sdk.read_data("get_waveforms")
+    if W is not None:
 
-                dt = 1.0
-                rising_edges = np.where(np.diff(nwave) > 500)[0]  # Indici dei fronti di salita con tolleranza del 10%
-                rise_times = np.zeros(len(rising_edges))  # Array per memorizzare i rise time
-                for i, edge in enumerate(rising_edges):
-                    # Trova il tempo in cui il segnale sale dal 10% al 90% del suo valore massimo con tolleranza del 10%
-                    threshold = 0.1 * (nwave[edge+10] - nwave[edge-10]) + nwave[edge-10]
-                    idx_start = np.where(nwave[edge-10:edge+10] >= threshold)[0][0] + edge-10
-                    threshold = 0.9 * (nwave[edge+10] - nwave[edge-10]) + nwave[edge-10]
-                    idx_end = np.where(nwave[edge-10:edge+10] >= threshold)[0][0] + edge-10
-                    rise_times[i] = (idx_end - idx_start) * dt  # Calcola il rise time
-                
-                # Calcola il tempo medio di salita
-                rise_time = np.average(rise_times)
-                #calcolo il periodo dell'onda di ingresso dai rising edges
-                period = np.average(np.diff(rising_edges[2:]))
-                frequency = 1/period * 10e9
-                print ("Channel %d" % ch)
-                print ("Average: %.2f" % avg)
-                print ("Peak to peak: %.2f" % p2p)
-                print ("RMS: %.2f" % rms)
-                print ("Frequency: %.2f" %frequency)
-                print("Rise time: %.2f ns" % rise_time)
+        for ch in range(0,8):
+            wave = W["wave"][ch]
+            nwave = np.array(wave)
+            # culate average, and averaged peak to peak
+            avg = np.average(nwave)
+            p2p = np.max(nwave) - np.min(nwave)
+        
+            rms = np.sqrt(np.mean(nwave **2))
+
+            dt = 1.0
+            rising_edges_a = np.where(np.diff(nwave) > 750)[0]  # Indici dei fronti di salita con tolleranza del 10%
+
+            # Lista degli indici da mantenere
+            keep_edges = [rising_edges_a[0]]
+
+            # Controllo della distanza minima tra gli edge
+            for i in range(1, len(rising_edges_a)):
+                if rising_edges_a[i] - keep_edges[-1] > 50:
+                    keep_edges.append(rising_edges_a[i])
+
+            # Nuova lista degli indici dei rising edge
+            rising_edges = np.array(keep_edges)
+
+            rise_times = np.zeros(len(rising_edges))  # Array per memorizzare i rise time
+            for i, edge in enumerate(rising_edges):
+                # Trova il tempo in cui il segnale sale dal 10% al 90% del suo valore massimo con tolleranza del 10%
+                threshold = 0.1 * (nwave[edge+10] - nwave[edge-10]) + nwave[edge-10]
+                idx_start = np.where(nwave[edge-10:edge+10] >= threshold)[0][0] + edge-10
+                threshold = 0.9 * (nwave[edge+10] - nwave[edge-10]) + nwave[edge-10]
+                idx_end = np.where(nwave[edge-10:edge+10] >= threshold)[0][0] + edge-10
+                rise_times[i] = (idx_end - idx_start) * dt  # Calcola il rise time
+            
+            # Calcola il tempo medio di salita
+            rise_time = np.average(rise_times)
+            #calcolo il periodo dell'onda di ingresso dai rising edges
+            period = np.average(np.diff(rising_edges[2:]))
+            frequency = 1/period * 10e9
+            print ("Channel %d" % ch)
+            print ("Average: %.2f" % avg)
+            print ("Peak to peak: %.2f" % p2p)
+            print ("RMS: %.2f" % rms)
+            print ("Frequency: %.2f" %frequency)
+            print("Rise time: %.2f ns" % rise_time)
 
 
 
+        waveforms = W["wave"]
 
+        # Creazione della figura
+        fig, axs = plt.subplots(8, 1, figsize=(8, 16))
+
+        # Disegno delle forme d'onda
+        for ch in range(8):
+            axs[ch].plot(waveforms[ch])
+            axs[ch].set_title("Canale {}".format(ch))
+            axs[ch].set_xlabel("Tempo")
+            axs[ch].set_ylabel("Ampiezza")
+
+        # Mostra il grafico
+        plt.show()
 
 
     i=i+1
